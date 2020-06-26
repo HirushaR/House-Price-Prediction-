bf6bf8ce23f84f9cf1a4f265a1f3d37078df9bb2
HirushaR <hirusharandunu11@gmail.com>
2020-06-26 13:46:04 +0530

    divied by data type

--- predict.py
+++ predict.py
@@ -1,3 +1,16 @@
 import pandas as pd
 
 df = pd.read_csv('data/train.csv')
+
+#print(df.info())
+# check how many column with null values
+#print(sum(df.isna().any()))
+
+#print(df['MasVnrType'].value_counts())
+
+#select all the columns with object data type
+list_obj_columns = list(df.select_dtypes(include='object').columns)
+
+#select all the columns data type isnt object
+list_num_columns = list(df.select_dtypes(exclude='object').columns)
+print(len(list_num_columns))
