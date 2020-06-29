5f07d2f38e4cb00645ff77a22dfff92f18c16582
HirushaR <hirusharandunu11@gmail.com>
2020-06-29 17:17:10 +0530

    load the data

--- predict.py
+++ predict.py
@@ -4,3 +4,14 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn import linear_model
+
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+
+print(train.shape)
+print(test.shape)
+
+plt.style.use(style='ggplot')
+plt.rcParams['figure.figsize'] = (10,6)
+
+print(train.SalePrice.describe())
